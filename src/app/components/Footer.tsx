bb0c52a012488fb75d68d55e78469b6ebe014583
BIftu Shibbire <109244924+Bonittas@users.noreply.github.com>
2023-07-20 20:07:01 +0300

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,141 +1,98 @@
-function Footer(){
-    return(
-<footer className="light-footer skin-light-footer style-2 bg-white z-30">
- <div className="footer-middle mx-48">
-  <div className="container my-16 justify-center ">
-    <div className="flex flex-col lg:flex-row justify-between">
-      <div className="w-full mb-8 lg:mb-0 lg:px-0">
-        <div className="footer_widget">
-          <img src="/images/logo.png" className=" mb-2" alt="Logo" />
-          <div className="address my-6 whitespace-nowrap">
-            7742 Sadar Street Range Road, USA<br />United Kingdom GHQ11
+const Footer = () => {
+  return (
+    <footer className="bg-white">
+      <div className="container mx-4 py-16">
+        <div className="flex flex-wrap justify-start lg:mx-10">
+          <div className="w-full lg:w-1/5 relative lg:left-20  mb-8 lg:mb-0 lg:px-4">
+            <img src="/images/logo.png" className="mb-8" alt="Logo" />
+            <div className="text-gray-600 mb-4">
+              7742 Sadar Street Range Road, USA<br />United Kingdom GHQ11
+            </div>
+            <div className="text-gray-600 mb-4">
+              40 568 423 6597<br />support@Goodup.com
+            </div>
           </div>
-          <div className="address mt-3">
-            40 568 423 6597<br />support@Goodup.com
+          <div className="w-full lg:w-1/6 mb-8 lg:mb-0 lg:ml-48 lg:px-4">
+            <h4 className="text-black font-bold mb-4">Main Navigation</h4>
+            <ul className="list-unstyled mb-1">
+              <li className="mb-1">
+                <a href="#">Explore Listings</a>
+              </li>
+              <li className="mb-1">
+                <a href="#">Browse Authors</a>
+              </li>
+              <li className="mb-1">
+                <a href="#">Submit Listings</a>
+              </li>
+              <li className="mb-1">
+                <a href="#">Shortlisted</a>
+              </li>
+              <li className="mb-1">
+                <a href="#">Dashboard</a>
+              </li>
+            </ul>
           </div>
-          <div className="address mt-2">
-            <ul className="list-inline">
-              <li className="list-inline-item">
-                <a href="#" className="theme-cl">
-                  <i className="lni lni-facebook-filled"></i>
-                </a>
+          <div className="w-full lg:w-1/6 mb-8 lg:mb-0 lg:px-4">
+            <h4 className="text-black font-bold mb-4">Business Owners</h4>
+            <ul className="list-unstyled mb-1">
+              <li className="mb-1">
+                <a href="#">Browse Categories</a>
               </li>
-              <li className="list-inline-item">
-                <a href="#" className="theme-cl">
-                  <i className="lni lni-twitter-filled"></i>
-                </a>
+              <li className="mb-1">
+                <a href="#">Payment Links</a>
               </li>
-              <li className="list-inline-item">
-                <a href="#" className="theme-cl">
-                  <i className="lni lni-youtube"></i>
-                </a>
+              <li className="mb-1">
+                <a href="#">Saved Places</a>
               </li>
-              <li className="list-inline-item">
-                <a href="#" className="theme-cl">
-                  <i className="lni lni-instagram-filled"></i>
-                </a>
+              <li className="mb-1">
+                <a href="#">Dashboard</a>
               </li>
-              <li className="list-inline-item">
-                <a href="#" className="theme-cl">
-                  <i className="lni lni-linkedin-original"></i>
-                </a>
+            </ul>
+          </div>
+          <div className="w-full lg:w-1/6 mb-8 lg:mb-0 lg:px-4 lg:order-last">
+            <h4 className="text-black font-bold mb-4">Helpful Topics</h4>
+            <ul className="list-unstyled mb-1">
+              <li className="mb-1">
+                <a href="#">SiteMap</a>
+              </li>
+              <li className="mb-1">
+                <a href="#">Security</a>
+              </li>
+              <li className="mb-1">
+                <a href="#">Contact</a>
+              </li>
+              <li className="mb-1">
+                <a href="#">FAQ's Page</a>
+              </li>
+              <li className="mb-1">
+                <a href="#">Privacy</a>
+              </li>
+            </ul>
+          </div>
+          <div className="w-full lg:w-1/6 mb-8 lg:mb-0 lg:px-4">
+            <h4 className="text-black font-bold mb-4">About Company</h4>
+            <ul className="list-unstyled mb-1">
+              <li className="mb-1">
+                <a href="#">Who We Are?</a>
+              </li>
+              <li className="mb-1">
+                <a href="#">Our Mission</a>
+              </li>
+              <li className="mb-1">
+                <a href="#">Our Team</a>
+              </li>
+              <li className="mb-1">
+                <a href="#">Packages</a>
+              </li>
+              <li className="mb-1">
+                <a href="#">Dashboard</a>
               </li>
             </ul>
           </div>
         </div>
       </div>
-      <div className="w-full mb-8 lg:mb-0 left-16 relative pr-6 flex justify-center ">
-        <div className="whitespace-nowrap relative mr-24">
-          <h4 className="  py-3 font-bold">Main Navigation</h4>
-          <ul className="mx-2">
-            <li className="py-3">
-              <a href="#">Explore Listings</a>
-            </li>
-            <li className="py-3">
-              <a href="#">Browse Authors</a>
-            </li>
-            <li className="py-3">
-              <a href="#">Submit Listings</a>
-            </li>
-            <li className="py-3">
-              <a href="#">Shortlisted</a>
-            </li>
-            <li className="py-3">
-              <a href="#">Dashboard</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className="w-full mb-8 lg:mb-0 lg:px-0 relative pr-6">
-        <div className="whitespace-nowrap">
-          <h4 className="widget_title py-3 font-bold">Business Owners</h4>
-          <ul className="mx-2">
-            <li className="py-3">
-              <a href="#">Browse Categories</a>
-            </li>
-            <li className="py-3">
-              <a href="#">Payment Links</a>
-            </li>
-            <li className="py-3">
-              <a href="#">Saved Places</a>
-            </li>
-            <li className="py-3">
-              <a href="#">Dashboard</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className="w-full mb-8 lg:mb-0 lg:px-0 relative pr-6">
-        <div className="whitespace-nowrap">
-          <h4 className="widget_title py-3 font-bold">About Company</h4>
-          <ul className="mx-2">
-            <li className="py-3">
-              <a href="#">Who We Are?</a>
-            </li>
-            <li className="py-3">
-              <a href="#">Our Mission</a>
-            </li>
-            <li className="py-3">
-              <a href="#">Our Team</a>
-            </li>
-            <li className="py-3">
-              <a href="#">Packages</a>
-            </li>
-            <li className="py-3">
-              <a href="#">Dashboard</a>
-            </li>
-          </ul>
-         </div>
-       </div>
+    </footer>
+  );
+};
 
-       <div className="w-full mb-8 lg:mb-0 lg:px-0 relative pr-6 whitespace-nowrap">
-         <div className="whitespace-nowrap">
-           <h4 className=" text-black font-24 py-3 font-bold text-10">Helpful Topics</h4>
-        <ul className="mx-2 ">
-            <li className="py-3">
-              <a href="#">Site Map</a>
-            </li>
-            <li className="py-3">
-              <a href="#">Security</a>
-            </li>
-            <li className="py-3">
-              <a href="#">Contact</a>
-            </li>
-            <li className="py-3">
-              <a href="#">FAQ's Page</a>
-            </li>
-            <li className="py-3">
-              <a href="#">Privacy</a>
-            </li>
-        </ul>
-      </div>
-    </div>
-   </div>
-  </div>
- </div>
-</footer>
-
-    )
-}
-
-export default Footer
+export default Footer;
